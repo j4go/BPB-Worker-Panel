4528da92378e9a276a515cfa460c07693769ebbb
j4 <yianny@163.com>
2024-10-01 15:31:13 +0800

    Update _worker.js uuid proxyip

--- _worker.js
+++ _worker.js
@@ -10,7 +10,7 @@ import { connect } from 'cloudflare:sockets';
 let userID = 'c1410a68-da2c-45e9-a937-a13db9df64ab';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
-const proxyIPs= ['bpb.yousef.isegaro.com'];
+const proxyIPs= ['cdn.xn--b6gac.eu.org','cdn-all.xn--b6gac.eu.org'];
 const defaultHttpPorts = ['80', '8080', '2052', '2082', '2086', '2095', '8880'];
 const defaultHttpsPorts = ['443', '8443', '2053', '2083', '2087', '2096'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
